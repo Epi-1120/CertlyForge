1625e4a57a56826d3ffdc67413c5fc2a9591f1bd
Epi Lo <epi-1120@users.noreply.github.com>
2025-11-12 09:41:18 +0000

    chore: add TypeScript config and ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/consistent-type-imports': 'warn',
+  },
+  ignorePatterns: ['dist', 'node_modules', 'test/fixtures'],
+};
