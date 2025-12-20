fd2a813b7db87fb91347eb22b9a5ff99ea7ae160
Epi-1120 <128920892+Epi-1120@users.noreply.github.com>
2025-12-20 18:08:14 +0800

    add security+ questions (30)

--- src/data/exams.js
+++ src/data/exams.js
@@ -1,4 +1,5 @@
 ﻿import { cisaExam } from './cisa.js'
 import { cismExam } from './cism.js'
-export const exams = [cisaExam, cismExam]
+import { secplusExam } from './secplus.js'
+export const exams = [cisaExam, cismExam, secplusExam]
 export function getExam(id) { return exams.find(e => e.id === id) }
